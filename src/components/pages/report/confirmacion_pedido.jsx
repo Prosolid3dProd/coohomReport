aa154904b0e304212ff006478c94c24b256c7df8
Álvaro <alvaroorti7@gmail.com>
2024-10-08 13:25:02 +0200

    acabado y armazon

--- src/components/pages/report/confirmacion_pedido.jsx
+++ src/components/pages/report/confirmacion_pedido.jsx
@@ -1602,7 +1602,7 @@ const Confirmacion_Pedido = ({ data, price, title }) => {
                               "PVA",
                               "PVL",
                               "ANCHO PUERTA",
-                              "MATERIAL PERFIL",
+                              "ACABADO PERFIL",
                               "PIES",
                               "VUELO IZQUIERDO",
                               "VUELO DERECHO",
