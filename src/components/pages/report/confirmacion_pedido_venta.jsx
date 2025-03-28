97f2d8fb7e4b11490c7e489dc08f942669a5fa89
Alvaro <alvaro@prosolid3d.com>
2025-03-28 14:34:04 +0100

    f regleta

--- src/components/pages/report/confirmacion_pedido_venta.jsx
+++ src/components/pages/report/confirmacion_pedido_venta.jsx
@@ -1049,7 +1049,7 @@ const Confirmacion_Pedido = ({
                       {item.name.toLocaleUpperCase().includes("REGLETA")
                         ? 150
                         : item.size.x}
-                      F: {item.size?.y} A: {item.size?.z}
+                       F: {item.size?.y} A: {item.size?.z}
                     </Text>
                     {item.opening && (
                       <Text style={{ fontSize: "8" }}>M: {item?.opening}</Text>
